8e9d8c1369a8b186af05585892757b1f8cd623ff
Gregory Lemieux <gregory.lemieux@gmail.gov>
2019-10-21 10:20:02 -0700

    changed version check to attribute error check

--- acre_history_utils.py
+++ acre_history_utils.py
@@ -89,29 +89,22 @@ def define_histvars(xmlfile,hdims,file0,n_htypes,test_name,base_name):
 
         fp = netcdf.netcdf_file(file0, 'r', mmap=False)
 
-        # This seems like a clunky work around
-        if sys.version_info[0] == 2:
-            if (fp.variables.has_key(name)):
+        # Check if the variable is found in the netcdf file
+        try:
+            fpkeycheck = fp.variables.has_key(name) # Python2
+        except AttributeError:
+            fpkeycheck = name in fp.variables # Python3
 
-                # Determine dimension info
-                dimnames = fp.variables[name].dimensions
-    #            units    = fp.variables[name]._attributes['units']
-                hvarlist.append(hist_vars(name,atypes,units,mults,offs, \
-                                          hdims,dimnames,hfile_id,n_htypes,test_name,base_name))
-            else:
-                print('History variable: '+name+', was not found in the history files')
+        if (fpkeycheck):
 
-        elif sys.version_info[0] == 3:
-            if (name in fp.variables): # Is this valid in python2.7
+            # Determine dimension info
+            dimnames = fp.variables[name].dimensions
+#            units    = fp.variables[name]._attributes['units']
+            hvarlist.append(hist_vars(name,atypes,units,mults,offs, \
+                                        hdims,dimnames,hfile_id,n_htypes,test_name,base_name))
+        else:
+            print('History variable: '+name+', was not found in the history files')
 
-                # Determine dimension info
-                dimnames = fp.variables[name].dimensions
-    #            units    = fp.variables[name]._attributes['units']
-                hvarlist.append(hist_vars(name,atypes,units,mults,offs, \
-                                          hdims,dimnames,hfile_id,n_htypes,test_name,base_name))
-            else:
-                print('History variable: '+name+', was not found in the history files')
-        
         fp.close()
 
     return(hvarlist)
